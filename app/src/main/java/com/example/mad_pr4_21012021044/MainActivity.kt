14ca9daae10b3f4a0c462c7b2ba02897ca1d03f0
Admin <ashvinikushwaha189@gmail.com>
2023-08-13 00:49:55 +0530

    ok

--- app/src/main/java/com/example/mad_pr4_21012021044/MainActivity.kt
+++ app/src/main/java/com/example/mad_pr4_21012021044/MainActivity.kt
@@ -10,8 +10,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var loginBtn = findViewById<Button>(R.id.login_button)
+        var registerBtn = findViewById<Button>(R.id.register_button)
+
         loginBtn.setOnClickListener {
             Intent(this@MainActivity,LoginActivity::class.java)
+                .also { startActivity(it) }
+        }
+        registerBtn.setOnClickListener {
+            Intent(this@MainActivity,RegisterActivity::class.java)
                 .also { startActivity(it) }
         }
     }
